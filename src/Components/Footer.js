10f95c0611ee16183ea70088c8077a629381b3e7
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2025-05-20 19:56:18 +0500

    updated footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ import { GrMail } from "react-icons/gr";
 const Footer = () => {
   return (
     <footer>
-        <h4>Copyright &copy; 2024</h4>
+        <h4>Copyright &copy; 2025</h4>
         <div className="footerLinks">
           <a href="https://github.com/MuhammadAmmarAtique" target="_blank">
             <FaGithub />
@@ -13,7 +13,7 @@ const Footer = () => {
           <a href="https://www.linkedin.com/in/muhammad-ammar-atique-0b409223a/" target="_blank">
             <FaLinkedin />
           </a>
-          <a href="mailTo:muhammadammaratique@gmail.com" target="_blank">
+          <a href="mailTo:muhammadammaratique@yahoo.com" target="_blank">
             <GrMail />
           </a>
           <a href="https://x.com/AmmarAtiqu" target="_blank">
