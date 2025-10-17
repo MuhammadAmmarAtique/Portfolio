dc5dc0cb2538766864a71ba66f87076d1cc079f9
Ammar Atiq <ammar.pixako1@gmail.com>
2025-10-17 12:31:41 +0500

    registered service worker file with my app so that it can run offline

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration.js";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
+
+// Register service worker (production builds only)
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    // Optionally prompt user to refresh
+    console.log("SW updated", registration);
+  },
+}).catch(error => {
+  console.error('SW registration failed:', error);
+});
